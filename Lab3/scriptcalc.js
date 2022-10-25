8b0ec0da94a79d8080e937934f768d4a74bc4980
ronan <rvalarese@gmail.com>
2022-10-24 21:16:20 -0500

    added JavaScript

---
+++ Lab3/scriptcalc.js
@@ -0,0 +1,20 @@
+const sliders = document.querySelectorAll("input[type='range']");
+const billInput = document.getElementById("cost");
+
+sliders.forEach(function(slider){
+  slider.addEventListener("input",calcu);
+});
+
+billInput.addEventListener("change",calcu);
+
+function calcu() {
+        let bill = document.forms["calculator"]["cost"].value;
+
+        console.log("my cock");
+        let tipPerc = document.forms["calculator"]["tip"].value;
+        document.getElementById("tipPercent").value = tipPerc;
+        let tipAmount = (bill / 100) * tipPerc;
+        document.getElementById("tipDollar").value = tipAmount.toFixed(2);
+        let total = +bill + tipAmount;
+        document.getElementById("totalTip").value = total.toFixed(2);
+      }
